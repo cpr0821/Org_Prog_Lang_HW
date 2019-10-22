61a42e6d54295c896a98ee763d524ccfb52d3b58
Camryn Rogers <cpr0821@gmail.com>
2019-10-22 14:29:05 -0500

    Add files via upload

---
+++ Q2.java
@@ -0,0 +1,20 @@
+
+package q2;
+
+import java.util.*;
+import java.io.*;
+
+public class Q2 {
+    public static void main(String[] args) {
+       for(int i = 0; i < 7; i++){
+           int a = 17;
+       }
+       try{
+           a = 12;
+           System.out.println("I can access a!");
+       }catch (Exception e){
+           System.out.println("I cannot access a :(");
+       }
+    }
+    
+}
